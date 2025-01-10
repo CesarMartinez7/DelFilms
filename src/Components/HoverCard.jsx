d5a60df4b28290ae995d49df404c21f8e1c917bd
CesarMartinez <cesarwamartinez@gmail.com>
2025-01-10 05:30:35 -0500

    se puso mejor fuente

--- src/Components/HoverCard.jsx
+++ src/Components/HoverCard.jsx
@@ -1,28 +1,38 @@
-import { useNavigate } from "react-router-dom"
-import NoImage from "../assets/noImage.webp"
+import { useNavigate } from "react-router-dom";
+import NoImage from "../assets/noImage.webp";
 import { Spoiler } from "spoiled";
 
-
-
-
-export default function HoverCard({data}) {
+export default function HoverCard({ data }) {
   const handleAnchor = (e) => {
     navigate(`/movie/${data?.id}`);
-  }
+  };
   const navigate = useNavigate();
   return (
-    <a className="relative overflow-hidden shadow-lg group rounded-2xl p-0.5 z-10" onClick={handleAnchor}>
-    <img
-      src={data?.poster_path === null || undefined ? NoImage  :`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${data?.poster_path}` }
-      alt={data?.title}
-      className="w-full h-full z-0 transition-transform duration-300 ease-in-out group-hover:scale-110 object-cover max-w-full max-h-full rounded-2xl"
-    />
-    <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black/100 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-      <div className="p-4 text-white">
-      <h3 className="text-sm md:text-md font-semibold mb-2">{data?.title}</h3>
-      <p className="text-sm flex justify-between"><span>{data?.release_date}</span></p>
+    <a
+      className="relative overflow-hidden shadow-lg group rounded-2xl p-0.5 z-10"
+      onClick={handleAnchor}
+    >
+      <img
+        src={
+          data?.poster_path === null || undefined
+            ? NoImage
+            : `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${data?.poster_path}`
+        }
+        alt={data?.title}
+        className="w-full h-full z-0 transition-transform duration-300 ease-in-out group-hover:scale-110 object-cover max-w-full max-h-full rounded-2xl"
+      />
+      <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black/100 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div className="p-4 text-white">
+          <h3 className="text-sm md:text-md font-semibold mb-2">
+            {data?.title}
+          </h3>
+          <div className="flex justify-between">
+            <p className="text-[10px]">
+              <span>{data?.release_date}</span>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
     </a>
-  )
+  );
 }

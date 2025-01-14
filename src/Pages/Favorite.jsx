9c36c00af297855d92cf7f7a1d814f422cb75ba0
CesarMartinez <cesarwamartinez@gmail.com>
2025-01-14 08:37:10 -0500

    se estaba añadiendo el boton de elliminar en el favorite page todo con localstorage

--- src/Pages/Favorite.jsx
+++ src/Pages/Favorite.jsx
@@ -22,19 +22,19 @@ export default function Favorite({ arrayLocalStorage }) {
   const [loaded, setLoaded] = useState(false);
   const [firstSession,setFirstSession] = useState(null)
 
+  const navigate = useNavigate()
 
-
-  useEffect(()=>{
-    if(localStorage.getItem("isFirsSession") === "false"){
-      setFirstSession(false)
-      console.log("Es false")
-      localStorage.setItem("isFirsSession", "true")
-    }else{
-      console.log("Es true")
-      setFirstSession(true)
-    }
+  // useEffect(()=>{
+  //   if(localStorage.getItem("isFirsSession") === "false"){
+  //     setFirstSession(false)
+  //     console.log("Es false")
+  //     localStorage.setItem("isFirsSession", "true")
+  //   }else{
+  //     console.log("Es true")
+  //     setFirstSession(true)
+  //   }
     
-  },[])
+  // },[])
 
   const handleFetching = async (movie) => {
     const URL = `https://api.themoviedb.org/3/movie/${movie}`;
@@ -42,7 +42,6 @@ export default function Favorite({ arrayLocalStorage }) {
       const respuesta = await fetch(URL, opciones);
       const data = await respuesta.json();
       setFetching((prev) => {
-        // Evitar duplicados verificando si el ID ya está en el estado
         if (!prev.find((item) => item.id === data.id)) {
           return [...prev, data];
         }
@@ -81,21 +80,32 @@ export default function Favorite({ arrayLocalStorage }) {
           <h3 className="font-semibold text-2xl my-7">Tus Favoritos</h3>
           <ul className="p-4 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 2xl:grid-cols-8 md:gap-3 gap-1">
             {fetching.map((movie) => (
-              <a
+              <div
                 key={movie?.id}
-                className=" p-2 rounded"
-                href={`/movie/${movie?.id}`}
+                className=" p-2 rounded bg-black"
               >
                 <img
                   src={movie.poster_path === null ? noImage : `https://image.tmdb.org/t/p/w500${movie?.poster_path}` }
                   alt={movie.title}
                   className="w-full h-auto rounded-2xl"
+                  onClick={() => {
+                    navigate(`/movie/${movie?.id}`)
+                  }}
                 />
                 <h4 className="mt-2 text-center font-semibold">
                   {movie?.title}
                 </h4>
-                
-              </a>
+                <button className="btn" onClick={() => {
+
+                  let resultado = fetching.filter((result) => result.id !== movie?.id)
+                  setFetching(resultado)
+                  const ids = new Array
+                  resultado.map((list) => ids.push(list?.id))
+                  localStorage.setItem("movieFavorite", JSON.stringify(ids))
+                  
+                }
+                }>Delete</button>
+              </div>
             ))}
           </ul>
         </div>

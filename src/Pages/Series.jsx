670ab90134ece78a06f40e06558290c849e2d924
CesarMartinez <cesarwamartinez@gmail.com>
2025-01-15 06:05:34 -0500

    interfaz de series mejorada

--- src/Pages/Series.jsx
+++ src/Pages/Series.jsx
@@ -1,48 +1,87 @@
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import HoverCard from "../Components/HoverCard";
+import NoImage from "../assets/noImage.webp";
 
+const HoverCardSeries = ({ show }) => {
+  const navigate = useNavigate();
+  return (
+    <a
+      className="relative overflow-hidden shadow-lg group rounded-2xl p-0.5 z-10"
+      onClick={() => {
+        navigate(`/series/${show?.id}`);
+      }}
+    >
+      <img
+        src={
+          show?.poster_path === null || undefined
+            ? NoImage
+            : `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${show?.poster_path}`
+        }
+        alt={show?.name}
+        className="w-full h-full z-0 transition-transform duration-300 ease-in-out group-hover:scale-110 object-cover max-w-full max-h-full rounded-2xl grayscale-1 "
+      />
+      <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black/100 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div className="p-4 text-white">
+          <h3 className="text-sm md:text-md font-semibold mb-2">
+            {show?.name}
+          </h3>
+          <div className="flex justify-between">
+            <p className="text-[10px]">
+              <span>{show?.first_air_date}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+};
 
 export default function Series() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const inputRef = useRef();
-  const [query,setQuery] = useState("")
+  const [query, setQuery] = useState(localStorage.getItem("serie_query") || "");
   const URL = `https://api.themoviedb.org/3/search/tv?query=${query}&api_key=46bc83931796202fd8d9924bf15987dd`;
-  const [data, setData] = useState([]);
+  const [show, setshow] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setQuery(e.target[0].value)
-    console.log(e.target[0].value)
+    localStorage.setItem("serie_query", e.target[0].value);
+    setQuery(localStorage.getItem("serie_query"));
+    console.log(
+      `Este es el query y viene de localStorage${localStorage.getItem(
+        "serie_query"
+      )}`
+    );
   };
 
   useEffect(() => {
     fetch(URL)
       .then((respuesta) => respuesta.json())
-      .then((dataa) => setData(dataa.results));
+      .then((showa) => setshow(showa.results));
   }, [query]);
 
   return (
     <>
-      <div>
-        <h1>HELLO SERIES API</h1>
+      <div className="mt-3">
+        <h1 className="font-bold text-2xl">Series router</h1>
         <form onSubmit={handleSubmit}>
-          <input type="text" ref={inputRef} className="back p-2 " name="query" />
+          
+          <input
+            ref={inputRef}
+            autoComplete="true"
+            element="input"
+            name="query"
+            type="text"
+            placeholder="Star Wars, Squid Games..."
+            className={
+              "back p-1 px-3 rounded-lg ring-none outline-none focus:ring-0 w-72 min-w-60 max-w-52"
+            }
+          />
         </form>
         <ul className="grid grid-cols-10 gap-4  p-4">
-          {data &&
-            data.map((movie) => (
-              <li key={movie?.id} onClick={() => {
-                navigate(`/series/${movie?.id}`)
-              }} >
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}.jpg`}
-                  alt=""
-                  className="rounded-lg"
-                />
-                <h2>{movie?.name}</h2>
-              </li>
-            ))}
+          {show &&
+            show.map((sh) => <HoverCardSeries show={sh}></HoverCardSeries>)}
         </ul>
       </div>
     </>
--- src/Pages/SeriesSearch.jsx
+++ src/Pages/SeriesSearch.jsx
@@ -102,19 +102,19 @@ export default function SeriesSearch() {
         <div>
           <details class="dropdown">
             <summary class="btn m-1 back">Seasons</summary>
-            <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <ul class="menu dropdown-content back  rounded-box z-[1] w-52 p-2 shadow">
               {show?.seasons.map((season) => (
-                <li className="btn"
+                <li className="text-left"
                   onClick={() => {
                     setSeasons(season.season_number);
-                    window.alert(seasons);
                   }}
                 >
-                  {season?.season_number}
+                  <button>Temporada {season.season_number}</button>
                 </li>
               ))}
             </ul>
           </details>
+          
         </div>
         <iframe
           src={`https://vidlink.pro/tv/${show?.id}/${seasons}/${incrementCap}`}

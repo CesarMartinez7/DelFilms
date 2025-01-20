2395dca6d460a1bc16df6e2ee51e9a520403a3f1
cesarubuntu <cesarwamartinez@gmail.com>
2025-01-20 18:04:07 -0500

    se añadieron 3 servidores mas a peliculas

--- src/Components/MovieServer.jsx
+++ src/Components/MovieServer.jsx
@@ -8,15 +8,23 @@ export default function MovieServer() {
   const { id } = useParams();
   return (
     <div className="grid h-screen place-items-center content-center">
-    <section className="flex flex-col flex-shrink justify-center back items-center min-w-fit p-5 gap-4 shadow-2xl bg-zinc-950 rounded-2xl">
-      <h3 className="font-light text-5xl p-6 text-center text-wrap">Servidores disponibles</h3>
+    <section className="flex flex-col flex-shrink justify-center back items-center  min-w-fit  p-10 gap-4 shadow-2xl bg-zinc-950 rounded-2xl">
+      <h3 className="font-light text-5xl  text-center text-wrap">Servidores disponibles</h3>
+      <p className="font-extralight text-[14px]">Pruebe el servidor que mas le agrade</p>
       <div className="inline-flex gap-3">
         <Link
           className="flex flex-shrink flex-grow flex-row items-center glass btn btn-neutral gap-2 rounded-md"
           to={`https://vidlink.pro/movie/${id}`}
         >
           <Icon icon="solar:play-line-duotone" width="16" height="16" />
-          Vid Link
+          VidLink Pro
+        </Link>
+        <Link
+          className="inline-flex items-center glass btn btn-neutral gap-2 rounded-md"
+          to={`https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=false`}
+        >
+          <Icon icon="solar:play-line-duotone" width="16" height="16" />
+          VidSrCC Link
         </Link>
         <Link
           className="inline-flex items-center glass btn btn-neutral gap-2 rounded-md"
@@ -30,13 +38,21 @@ export default function MovieServer() {
           to={`https://vidsrc.to/embed/movie/${id}`}
         >
           <Icon icon="solar:play-line-duotone" width="16" height="16" />
-          VidSrc Link
+          VidSrcTo Link
         </Link>
+        <Link
+          className="inline-flex items-center glass btn btn-neutral gap-2 rounded-md"
+          to={`https://vidsrc.icu/embed/movie/${id}`}
+        >
+          <Icon icon="solar:play-line-duotone" width="16" height="16" />
+          VidSrcICU Link
+        </Link>
+        
       </div>
       <div>
         {/* <Spoiler tagName="div">
           <img src={noImage} alt="" />
-        </Spoiler> */}
+        </Spoiler>  */}
       </div>
     </section>
     </div>
